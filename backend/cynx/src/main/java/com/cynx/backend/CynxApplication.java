c87e4f51992d449bed2861b50ff4a707d2faf5cf
Shubham <mr.shubhamgaikwad0910@gmail.com>
2025-03-03 18:54:24 +0530

    Fixed nested Git issue and added backend

---
+++ backend/cynx/src/main/java/com/cynx/backend/CynxApplication.java
@@ -0,0 +1,13 @@
+package com.cynx.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CynxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CynxApplication.class, args);
+	}
+
+}
---
+++ backend/cynx/src/test/java/com/cynx/backend/CynxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cynx.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CynxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
